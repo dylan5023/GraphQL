c5670fd33d4a59fee941376c717b0f5592b3ca9e
Dylan5023 <kangjun5023@gmail.com>
2023-09-11 17:21:49 -0700

    build up graphql tools

--- server.js
+++ server.js
@@ -1,54 +1,19 @@
 const express = require("express");
-
 const { graphqlHTTP } = require("express-graphql");
-const { buildSchema } = require("graphql");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { loadFilesSync } = require("@graphql-tools/load-files");
 
+const loadesTypes = loadFilesSync("**/*", {
+  extensions: ["graphql"],
+});
 // construct a schema, using GraphQL schema language
-const schema = buildSchema(`
-    type Query {
-        posts: [Post]
-        comments: [Comment]
-    }
-    type Post {
-        id:ID!
-        title: String!
-        description: String!
-        comments: [Comment]
-    }
-    type Comment {
-        id:ID!
-        text: String!
-        likes: Int
-    }
-    `);
+
+const schema = makeExecutableSchema({
+  typeDefs: loadesTypes,
+});
 const root = {
-  posts: [
-    {
-      id: "post1",
-      title: "It is a first post",
-      description: "It is a first post description",
-      comments: [
-        {
-          id: "comment1",
-          text: "It is a first comment",
-          likes: 1,
-        },
-      ],
-    },
-    {
-      id: "post2",
-      title: "It is a second post",
-      description: "It is a second post description",
-      comments: [],
-    },
-  ],
-  comments: [
-    {
-      id: "comment1",
-      text: "It is a first comment",
-      likes: 1,
-    },
-  ],
+  posts: require("./posts/posts.model"),
+  comments: require("./comments/comments.model"),
 };
 
 const app = express();
